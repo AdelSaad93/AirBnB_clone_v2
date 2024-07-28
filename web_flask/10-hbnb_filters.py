8cd6906e549b0ecc850a24de29c53e2bfa754c22
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2024-07-27 23:46:57 -0700

    var

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -9,13 +9,14 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
